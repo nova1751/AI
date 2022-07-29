c51539a69e7793332c4b12f9d495a5d895341ae0
nova1751 <nova2319427728@outlook.com>
2022-07-29 17:28:27 +0800

    更新

--- Basic Knowledge/Numpy/Terating over Array.py
+++ Basic Knowledge/Numpy/Terating over Array.py
@@ -55,3 +55,66 @@ c = b.copy(order='F')
 print (c)
 for x in np.nditer(c):
     print (x, end=", " )
+
+# 可以通过显式设置，来强制 nditer 对象使用某种顺序：
+a = np.arange(0,60,5)
+a = a.reshape(3,4)
+print ('原始数组是：')
+print (a)
+print ('\n')
+print ('以 C 风格顺序排序：')
+for x in np.nditer(a, order =  'C'):
+    print (x, end=", " )
+print ('\n')
+print ('以 F 风格顺序排序：')
+for x in np.nditer(a, order =  'F'):
+    print (x, end=", " )
+
+# 修改数组中元素的值
+# nditer 对象有另一个可选参数 op_flags。 默认情况下，nditer 将视待迭代遍历的数组为只读对象（read-only），
+# 为了在遍历数组的同时，实现对数组元素值得修改，必须指定 read-write 或者 read-only 的模式。
+a = np.arange(0,60,5)
+a = a.reshape(3,4)
+print ('原始数组是：')
+print (a)
+print ('\n')
+for x in np.nditer(a, op_flags=['readwrite']):
+    x[...]=2*x
+print ('修改后的数组是：')
+print (a)
+
+# 使用外部循环
+# nditer 类的构造器拥有 flags 参数，它可以接受下列值：
+#
+# 参数	描述
+# c_index	可以跟踪 C 顺序的索引
+# f_index	可以跟踪 Fortran 顺序的索引
+# multi_index	每次迭代可以跟踪一种索引类型
+# external_loop	给出的值是具有多个值的一维数组，而不是零维数组
+a = np.arange(0,60,5)
+a = a.reshape(3,4)
+print ('原始数组是：')
+print (a)
+print ('\n')
+print ('修改后的数组是：')
+for x in np.nditer(a, flags =  ['external_loop'], order =  'F'):
+   print (x, end=", " )
+
+# 广播迭代
+# 如果两个数组是可广播的，nditer 组合对象能够同时迭代它们。 假设数组 a 的维度为 3X4，数组 b 的维度为 1X4 ，
+# 则使用以下迭代器（数组 b 被广播到 a 的大小）。
+a = np.arange(0,60,5)
+a = a.reshape(3,4)
+print  ('第一个数组为：')
+print (a)
+print  ('\n')
+print ('第二个数组为：')
+b = np.array([1,  2,  3,  4], dtype =  int)
+print (b)
+print ('\n')
+print ('修改后的数组为：')
+for x,y in np.nditer([a,b]):
+    print ("%d:%d"  %  (x,y), end=", " )
+
+
+
